5fa0346dee399d5a3e5811a5f459ac6069a9a841
KevinSatrio <5026221083@mhs.its.ac.id>
2023-11-21 12:27:19 +0700

    Pertemuan 21/11/2023

--- routes/web.php
+++ routes/web.php
@@ -55,3 +55,16 @@ Route::get('/halo2', function () {
 Route::get('blog', function () {
 	return view('blog');
 });
+
+Route::get('showjam/{jam}', 'App\Http\Controllers\DosenController@showjam');
+Route::get('/formulir', 'App\Http\Controllers\DosenController@formulir');
+Route::post('/formulir/proses', 'App\Http\Controllers\DosenController@proses');
+
+
+//route CRUD
+Route::get('/pegawai','App\Http\Controllers\PegawaiController@index');
+Route::get('/pegawai/tambah','App\Http\Controllers\PegawaiController@tambah');
+Route::post('/pegawai/store','App\Http\Controllers\PegawaiController@store');
+Route::get('/pegawai/edit/{id}','App\Http\Controllers\PegawaiController@edit');
+Route::post('/pegawai/update','App\Http\Controllers\PegawaiController@update');
+Route::get('/pegawai/hapus/{id}','App\Http\Controllers\PegawaiController@hapus');
