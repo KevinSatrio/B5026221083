7b2b24d4be8d2da99454d2b134a72fd9f4520c2c
KevinSatrio <5026221083@mhs.its.ac.id>
2023-12-10 22:37:41 +0700

    -Optimization (view tasedit.blade.php)

--- resources/views/Tas/tasedit.blade.php
+++ resources/views/Tas/tasedit.blade.php
@@ -25,6 +25,8 @@
                             <input type="text" class="form-control" id="jabatan" name="jabatan" value="{{ $p->stocktas }}">
                         </div>
                     </div>
+
+                    @if ($p->tersedia == 'y')
                     <div class="row">
                         <div class="col-sm-4">
                             <label for="tersedia" class="col-form-label">Ketersediaan: </label>
@@ -51,6 +53,36 @@
                             <a href="/tasindex" class="btn btn-secondary ml-2">Kembali</a>
                         </div>
                     </div>
+                    @endif
+
+                    @if ($p->tersedia == 'n')
+                    <div class="row">
+                        <div class="col-sm-4">
+                            <label for="tersedia" class="col-form-label">Ketersediaan: </label>
+                        </div>
+                        <div class="col-sm-4">
+                            <div class="form-check-inline">
+                                <label class="form-check-label" for="tersedia">
+                                    <input type="radio" class="form-check-input" id="tersedia" name="tersedia" value="y"
+                                        >Tersedia
+                                </label>
+                            </div>
+                            <div class="form-check-inline">
+                                <label class="form-check-label" for="tersedia">
+                                    <input type="radio" class="form-check-input" id="tersedia" name="tersedia"
+                                        value="n" checked>Tidak
+                                    Tersedia
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <div class="col-sm-8 offset-sm-4">
+                            <button type="submit" class="btn btn-primary">Simpan Data</button>
+                            <a href="/tasindex" class="btn btn-secondary ml-2">Kembali</a>
+                        </div>
+                    </div>
+                    @endif
                 </form>
             </div>
         </div>
